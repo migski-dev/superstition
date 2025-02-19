5faaa21991b33de0fd83dfefeecb8e085a8c2242
mig <miguelv.dev@gmail.com>
2025-02-19 18:58:34 -0500

    added custom models, outline shaders, fixed collisions, and fixed level speed rate

--- scenes/game_objects/npc/npc.gd
+++ scenes/game_objects/npc/npc.gd
@@ -4,8 +4,16 @@ class_name NPC
 var is_going_right: bool = true
 var speed: int = 3
 
+var mesh_wrapper: Node3D 
+var anim_player:AnimationPlayer 
+
 func _ready() -> void:
+	mesh_wrapper = $SUPERSTITION_NPC
+	anim_player = $SUPERSTITION_NPC/AnimationPlayer
 	apply_floor_snap() 
+	anim_player.play("Walking", -1, 1.5)
+	if(is_going_right):
+		mesh_wrapper.rotate(Vector3.UP, PI)
 
 func _physics_process(delta):
 	if is_going_right:
--- scenes/map_generation/modules/_module.gd
+++ scenes/map_generation/modules/_module.gd
@@ -2,10 +2,12 @@ extends Node3D
 
 @onready var level = $"../"
 
+var speed = 3.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	position.x -= GameEvents.game_speed * delta
+	#position.x -= GameEvents.game_speed * delta
+	position.x -= speed * delta
 	if position.x < (-15):
 		level.spawn_module(position.x+(level.amount*level.offset))
 		queue_free()
