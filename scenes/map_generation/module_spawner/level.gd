98dc84ba1e5e9201e4175c1daf607d93749e5181
mig <miguelv.dev@gmail.com>
2025-02-19 22:15:42 -0500

    fixed module generation

--- scenes/map_generation/module_spawner/level.gd
+++ scenes/map_generation/module_spawner/level.gd
@@ -1,27 +1,38 @@
 extends Node3D
 
 @export var modules: Array[PackedScene] = []
-var amount: int = 50
+var amount: int = 12
 var rng:RandomNumberGenerator = RandomNumberGenerator.new()
-var offset: int = 6 # How far each module is
+var offset: float = 6.0 # How far each module is
 
 var init_obs = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	GameEvents.head_position = 0
+	GameEvents.initial_offset = amount*offset
 	for n in amount:
 		spawn_module(n*offset)
+		#spawn_module()
 
-func spawn_module(n: int) -> void:
+func spawn_module(n: float) -> void:
 	if init_obs > 3:
 		rng.randomize()
 		var num = rng.randi_range(1,modules.size()-1)
 		var instance:Node3D = modules[num].instantiate()
 		instance.position.x = n
+		#instance.global_transform.origin.x = GameEvents.head_module.global_transform.origin.x 
 		add_child(instance)
+		#GameEvents.head_module = instance
+
+		
 	else:
 		var instance = modules[0].instantiate()
 		instance.position.x = n
+		#instance.position.x = GameEvents.head_position
 		add_child(instance)
+		#GameEvents.head_module = instance
 		init_obs += 1
+	
+	GameEvents.head_position += GameEvents.module_offset
+	
+	
--- scenes/map_generation/modules/_module.gd
+++ scenes/map_generation/modules/_module.gd
@@ -2,12 +2,15 @@ extends Node3D
 
 @onready var level = $"../"
 
-var speed = 3.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	#position.x -= GameEvents.game_speed * delta
-	position.x -= speed * delta
-	if position.x < (-15):
+	position.x -= GameEvents.game_speed * delta
+	#if position.x < (GameEvents.sister.position.x -15):
+	if position.x < -15:
 		level.spawn_module(position.x+(level.amount*level.offset))
+		#if GameEvents.head_module.global_position.x >= self.global_position.x + GameEvents.initial_offset:
+			#print('new head powsition - ', GameEvents.head_position)
+			#GameEvents.head_module = level.spawn_module()
+			#print('new head powsition - ', GameEvents.head_position)
 		queue_free()
